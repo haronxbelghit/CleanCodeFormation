c9d0e0b819af151a3e79bba7717a46d4af5825f1
Haron Belghit <haronbel@gmail.com>
2023-03-23 19:26:44 +0000

    Added SplashScreen

--- app/src/main/java/com/sqli/cleancodeformation/presentation/view/activity/MainActivity.kt
+++ app/src/main/java/com/sqli/cleancodeformation/presentation/view/activity/MainActivity.kt
@@ -2,6 +2,7 @@ package com.sqli.cleancodeformation.presentation.view.activity
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupActionBarWithNavController
@@ -15,6 +16,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen()
         setContentView(R.layout.activity_main)
 
         val navHostFragment =
